97daf7f1474dedc9b847f49eefac7cc4c87d412f
adolhunter <mingshiwang123@gmail.com>
2020-07-02 18:10:56 -0500

    add media query for nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import "./styles/App.css";
-// import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Auth from "./pages/Auth";
 import Home from "./pages/Home";
@@ -9,7 +9,9 @@ import { UserContext } from "./contexts/UserContext";
 function App() {
   const { user } = useContext(UserContext);
   return <>
-  {user ? <Home /> : <Auth />} 
+  {/* {user ? <Home /> : <Auth />}  */}
+  <NavBar />
+  <Home />
   </>;
 }
 
