e030340b1716771254e2ff4b83f85e13a6d19e4f
adolhunter <mingshiwang123@gmail.com>
2020-07-03 21:22:24 -0500

    finish post UI

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -10,7 +10,7 @@ const ActionGroup = () => {
         alignItems: "center",
         // justifyContent: "space-around",
         height: "40px",
-        padding: "1rem 1rem 0.4rem",
+        padding: "1rem 1rem 0.2rem",
       }}
     >
       <div
@@ -28,10 +28,55 @@ const ActionGroup = () => {
   );
 };
 
+const CommentStatus = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: "0.3rem 1rem 1rem",
+      }}
+    >
+      <div style={{ fontWeight: "500" }}>3 likes</div>
+      <div
+        style={{
+          display: "block",
+          margin: "0.4em 0",
+        }}
+      >
+        <span style={{ fontWeight: "500" }}>frank</span> wow!
+      </div>
+      <div style={{ color: "rgb(178, 178, 178)" }}>4 days ago</div>
+    </div>
+  );
+};
+
+const AddComment = () => {
+  return (
+    <div>
+      <textarea
+        placeholder="Add a comment"
+        style={{
+          height: "100%",
+          width: "100%",
+          borderTop: "1px solid rgb(219, 219, 219)",
+          borderBottom: "none",
+          borderRight: "none",
+          padding: "1rem 0px 0px 1rem",
+          resize: "none",
+          borderLeftStyle: "none",
+        }}
+      ></textarea>
+    </div>
+  );
+};
+
 const Comment = () => {
   return (
     <div>
       <ActionGroup />
+      <CommentStatus />
+      <AddComment />
     </div>
   );
 };
